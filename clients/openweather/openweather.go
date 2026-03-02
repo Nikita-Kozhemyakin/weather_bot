89d67e4cad1f34f323cc27fbd40fa550cd9ace05
Nikita-Kozhemyakin <nikita_kozhemyakin1@mail.ru>
2026-03-02 17:18:55 +0300

    new

---
+++ clients/openweather/openweather.go
@@ -0,0 +1,21 @@
+package openweather
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type OpenWeatherClient struct {
+	apiKey string
+}
+
+func New(apiKey string) *OpenWeatherClient {
+	return &OpenWeatherClient{
+		apiKey: apiKey,
+	}
+}
+
+func (o OpenWeatherClient) Coordinates(city string) {
+	url := "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=5&appid=%s"
+	resp, err := http.Get(fmt.Sprintf(url, city, o.apiKey))
+}
